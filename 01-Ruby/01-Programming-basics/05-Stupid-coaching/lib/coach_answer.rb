6751c6932263847ab0914fafc90fbaa03d1c095f
Daniel Enriquez <daniel.enm17@gmail.com>
2022-01-25 23:24:02 -0600

    test passed

--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
@@ -10,8 +10,7 @@ def coach_answer(your_message)
 end
 
 def coach_answer_enhanced(your_message)
-  if your_message.upcase == "I am going to work right now!".upcase
-    ""
+  if your_message.upcase == "I am going to work right now!".upcase then ""
   elsif (your_message.include? "?") && (your_message == your_message.upcase)
     "I can feel your motivation! Silly question, get dressed and go to work!"
   elsif your_message == your_message.upcase
