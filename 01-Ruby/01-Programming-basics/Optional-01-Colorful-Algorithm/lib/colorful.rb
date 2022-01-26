e741349085e54ab791ff8c5202bdefb71b5457a5
Daniel Enriquez <daniel.enm17@gmail.com>
2022-01-26 01:15:21 -0600

    Proof

--- 01-Ruby/01-Programming-basics/Optional-01-Colorful-Algorithm/lib/colorful.rb
+++ 01-Ruby/01-Programming-basics/Optional-01-Colorful-Algorithm/lib/colorful.rb
@@ -1,4 +1,21 @@
 def colorful?(number)
   # TODO: return true if the number is colorful, false otherwise
+  for i in (0...number.size-1) do
+    num = number[i].to_i * number[i+1].to_i
+    number << num
+  end
+
+  i = 0
+  num = number[i].to_i * number[i+1].to_i * number[i+2].to_i
+  number << num
+
+  if number.map(&:to_i).uniq.size < number.size
+    return false   
+  else
+    return true
+  end
 end
+
+number = gets.chomp
+puts colorful?(number.split(""))
 
