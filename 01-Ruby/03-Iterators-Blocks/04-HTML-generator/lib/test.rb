235457cbbf02ba0f8c9628b425f2cf90348b9d5d
Daniel Enriquez <daniel.enm17@gmail.com>
2022-01-27 16:47:44 -0600

    tests passed

---
+++ 01-Ruby/03-Iterators-Blocks/04-HTML-generator/lib/test.rb
@@ -0,0 +1,7 @@
+def tag(tag_name, attributes = nil)
+    if attributes.nil?
+      "<#{tag_name}>#{yield}</#{tag_name}>"
+    else
+      "<#{tag_name} #{attributes[0]}=\"#{attributes[1]}\">#{yield}</#{tag_name}>"
+    end
+  end
