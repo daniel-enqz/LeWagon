f8b09092a3480c84a8993e3bd9c40c55380821b2
Daniel Enriquez <daniel.enm17@gmail.com>
2022-01-27 00:44:29 -0600

    tests passed

--- 01-Ruby/02-Flow-Array/06-Destructive-Methods/lib/horse_racing.rb
+++ 01-Ruby/02-Flow-Array/06-Destructive-Methods/lib/horse_racing.rb
@@ -1,7 +1,11 @@
-def horse_racing_format!(race_array)
-  # TODO: modify the given array so that it is horse racing consistent. This method should return nil.
+# reverse the array, prepend the horse position, and add a exclamation mark
+# TODO: modify the given array so that it is horse racing consistent. This method should return nil.
+def horse_racing_format!(horse_race)
+  horse_race.reverse!
+  position = 5
+  horse_race.each { |horse| horse.prepend("#{position -= 1}-") }
+  horse_race.map! { |x| "#{x}!" }
 end
 
-
-
-
+horse_race = ["Abricot du Laudot", "Black Caviar", "Brigadier Gerard", "Coup de Folie"]
+horse_racing_format!(horse_race)
