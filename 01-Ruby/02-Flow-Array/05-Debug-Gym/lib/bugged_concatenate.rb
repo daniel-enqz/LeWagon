0c75c7a3d29b1bc1432d14bd28e828030311576e
Daniel Enriquez <daniel.enm17@gmail.com>
2022-01-26 20:40:29 -0600

    test passed

--- 01-Ruby/02-Flow-Array/05-Debug-Gym/lib/bugged_concatenate.rb
+++ 01-Ruby/02-Flow-Array/05-Debug-Gym/lib/bugged_concatenate.rb
@@ -7,5 +7,6 @@ def bugged_concatenate(array)
 end
 
 def build_1984_title
-  bugged_concatenate(1, "9", 84, " ", "George Orwell")
+  array = ["1", "9", "84", " ", "George Orwell"]
+  bugged_concatenate(array)
 end
