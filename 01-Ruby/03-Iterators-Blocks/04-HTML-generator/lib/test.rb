74d3bdc05726b232c8601615b2a52760c813888e
Daniel Enriquez <daniel.enm17@gmail.com>
2022-01-28 00:28:25 -0600

    delted file

--- 01-Ruby/03-Iterators-Blocks/04-HTML-generator/lib/test.rb
+++
@@ -1,7 +0,0 @@
-def tag(tag_name, attributes = nil)
-    if attributes.nil?
-      "<#{tag_name}>#{yield}</#{tag_name}>"
-    else
-      "<#{tag_name} #{attributes[0]}=\"#{attributes[1]}\">#{yield}</#{tag_name}>"
-    end
-  end
