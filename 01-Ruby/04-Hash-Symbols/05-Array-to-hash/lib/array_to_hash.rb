44ad72cdd194d94909a9912e83900f7203141e09
Daniel Enriquez <daniel.enm17@gmail.com>
2022-01-29 17:44:48 -0600

    tests passed

--- 01-Ruby/04-Hash-Symbols/05-Array-to-hash/lib/array_to_hash.rb
+++ 01-Ruby/04-Hash-Symbols/05-Array-to-hash/lib/array_to_hash.rb
@@ -1,3 +1,9 @@
 def array_to_hash(array)
-  # TODO: implement the method :)
+  hash = {}
+  if block_given? # call it passing the array index and use what’s returned as the hash key.
+    array.each_with_index { |value, index| hash[yield(index)] = value }
+  else # then the hash keys should just be the indexes of the elements in the array, converted as String.
+    array.each_with_index { |value, index| hash[index.to_s] = value }
+  end
+  return hash
 end
