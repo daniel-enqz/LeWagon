f716d1f52d35162ab0cd4e18f690f89ccb7a2012
Daniel Enriquez <daniel.enm17@gmail.com>
2022-01-29 17:54:24 -0600

    tests passed

--- 01-Ruby/04-Hash-Symbols/05-Array-to-hash/lib/array_to_hash.rb
+++ 01-Ruby/04-Hash-Symbols/05-Array-to-hash/lib/array_to_hash.rb
@@ -1,6 +1,6 @@
 def array_to_hash(array)
   hash = {}
-  if block_given? 
+  if block_given?
     array.each_with_index { |value, index| hash[yield(index)] = value }
   else
     array.each_with_index { |value, index| hash[index.to_s] = value }
