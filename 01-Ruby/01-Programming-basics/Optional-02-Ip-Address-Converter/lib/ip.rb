e8f6d8e89094ae7fd4022bb3542c6efede3cf988
Daniel Enriquez <daniel.enm17@gmail.com>
2022-01-30 16:58:18 -0600

    1st test passed

--- 01-Ruby/01-Programming-basics/Optional-02-Ip-Address-Converter/lib/ip.rb
+++ 01-Ruby/01-Programming-basics/Optional-02-Ip-Address-Converter/lib/ip.rb
@@ -1,6 +1,18 @@
 def ip_to_num(ip_address)
-  # TODO: return the number version of the `ip_address` string
+  binary_notation = []
+  final_result = 0
+  until binary_notation.size == 4
+    binary_notation << ("%08b" % ip_address.to_i)
+    ip_address = ip_address.gsub(ip_address.to_i.to_s, "")[1..]
+  end
+  binary_notation.join.chars.each_with_index do |number, index|
+    final_result += (2**(30 - index + 1)) if number.to_i == 1
+  end
+  final_result
 end
+
+p ip_to_num("37.160.113.170")
+
 
 def num_to_ip(number)
   # TODO: return the string IP address from the `number`
