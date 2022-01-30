4eb175864d8a1beafdbac5b8da771a1fdbe8a447
Daniel Enriquez <daniel.enm17@gmail.com>
2022-01-30 01:59:39 -0600

    tests passed

--- 01-Ruby/01-Programming-basics/Optional-01-Colorful-Algorithm/lib/colorful.rb
+++ 01-Ruby/01-Programming-basics/Optional-01-Colorful-Algorithm/lib/colorful.rb
@@ -1,25 +1,15 @@
 def colorful?(number)
-  # TODO: return true if the number is colorful, false otherwise
   number = number.to_s.chars.map(&:to_i)
-  if number.size == 1
-    true
-  else
-    # Multiply all numbers inside the array and store them in num
-    num = number.inject(:*)
-    # Multiply subsequent index
-    arr = []
-    number.each_with_index do |n, index|
-      arr << (n[index] * n[index + 1])
-    end
-    print arr
-    print number
-    if number.uniq.size == number.size
-      true
-    else
-      number << num
-      print number
-      number.uniq.size == number.size
-    end
+  return true if number.size == 1
+
+  if number.size == 2
+    number << number.inject(:*)
+    return number.uniq.size == number.size
   end
+  num = number.inject(:*)
+  arr = []
+  number.each_with_index { |_n, index| arr << (number[index] * number[index + 1] if index < number.size - 1) }
+  arr.each_with_index { |n, _index| number << n unless nil? }
+  number << num
+  number.uniq.size == number.size
 end
-puts colorful?(236)
