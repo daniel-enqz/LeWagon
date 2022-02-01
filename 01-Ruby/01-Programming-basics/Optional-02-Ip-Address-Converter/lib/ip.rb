c7003b4fe0e56ddd736899841c778896ea06566a
Daniel Enriquez <daniel.enm17@gmail.com>
2022-02-01 10:43:12 -0600

    tests passed only 2 offenses needed

--- 01-Ruby/01-Programming-basics/Optional-02-Ip-Address-Converter/lib/ip.rb
+++ 01-Ruby/01-Programming-basics/Optional-02-Ip-Address-Converter/lib/ip.rb
@@ -7,12 +7,20 @@ def ip_to_num(ip_address)
   end
   binary_notation.join.chars.each_with_index do |number, index|
     final_result += (2**(30 - index + 1)) if number.to_i == 1
-  end
+  end 
   final_result
 end
-ip_to_num("37.160.113.170")
+#ip_to_num("37.160.113.170")
 
 
-def num_to_ip(number)
-  # TODO: return the string IP address from the `number`
+def num_to_ip(number)    
+  binary = ""
+  while(number != 0)
+      binary = String(number % 2) + binary
+      number = number / 2
+  end
+  binary 
 end
+
+p num_to_ip(3232235521)
+# "192.168.0.1"
