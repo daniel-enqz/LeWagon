286ff9285c698914eb11461e5816f757d3c4e4c7
Daniel Enriquez <daniel.enm17@gmail.com>
2022-02-02 13:58:53 -0600

    tests passed

--- 02-OOP/01-OO-Basics/03-Basic-OOP/lib/orange_tree.rb
+++ 02-OOP/01-OO-Basics/03-Basic-OOP/lib/orange_tree.rb
@@ -1,3 +1,9 @@
 class OrangeTree
   # TODO: Implement all the specs defined in the README.md :)
 end
+
+def one_year_passes!
+  # TODO: check if the tree has survived
+  # TODO: if so, make the tree grow
+  # TODO: and produce fruits
+end
