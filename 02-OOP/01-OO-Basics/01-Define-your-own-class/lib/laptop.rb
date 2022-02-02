735e07d0ae6b6b85770804a2edfce1da63f3e8dc
Daniel Enriquez <daniel.enm17@gmail.com>
2022-02-02 11:34:35 -0600

    tests passed

--- 02-OOP/01-OO-Basics/01-Define-your-own-class/lib/laptop.rb
+++ 02-OOP/01-OO-Basics/01-Define-your-own-class/lib/laptop.rb
@@ -1,12 +1,10 @@
 class Laptop
+  attr_reader :model
+
   def initialize(model, processor, ram, storage)
     @model = model
     @processor = processor
     @ram = ram
     @storage = storage
-  end
-
-  def model
-    return @model
   end
 end
