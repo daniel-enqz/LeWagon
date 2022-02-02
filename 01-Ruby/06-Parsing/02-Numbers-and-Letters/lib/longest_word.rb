c286bab77830035023dfadd5278278d25557ccc3
Daniel Enriquez <daniel.enm17@gmail.com>
2022-02-02 16:37:46 -0600

    tests passed

--- 01-Ruby/06-Parsing/02-Numbers-and-Letters/lib/longest_word.rb
+++ 01-Ruby/06-Parsing/02-Numbers-and-Letters/lib/longest_word.rb
@@ -6,34 +6,23 @@ require 'pry-byebug'
 def generate_grid(grid_size)
   range = ("A".."Z").to_a
   string = []
-  grid_size.times {string.push(range.sample)}
+  grid_size.times { string.push(range.sample) }
   string
 end
 
 def score_calculator(length, elapsed)
-  score = length
-  if elapsed < score
-    score - elapsed
-  elsif elapsed > score
-  end
+  length / elapsed
 end
 
 def run_game(attempt, grid, start_time, end_time)
-  hash = {score: 0, message: "", time: end_time - start_time }
-#--------------------------------------------------------
+  hash = { score: 0, message: "not in the grid", time: end_time - start_time }
   url = "https://wagon-dictionary.herokuapp.com/#{attempt}"
   user_serialized = URI.open(url).read
   user = JSON.parse(user_serialized)
-#--------------------------------------------------------
-  if user["found"] == true && attempt.chars.all? {|letter| attempt.count(letter) <= grid.count(letter.upcase)}
-    hash[:score] = (score_calculator(user["length"], hash[:time]))
+  if user["found"] == true && attempt.chars.all? { |letter| attempt.count(letter) <= grid.count(letter.upcase) }
+    hash[:score] = score_calculator(user["length"], hash[:time])
     hash[:message] = "well done"
-  else
-    if user["found"] == false
-      hash[:message] = "not an english word"
-    elsif attempt.chars.all? {|letter| attempt.count(letter) <= grid.count(letter.upcase)} == false
-      hash[:message] = "not in the grid"
-    end
   end
-hash
+  hash[:message] = "not an english word" if user["found"] == false
+  hash
 end
