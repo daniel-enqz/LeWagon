d8f7d9d5a4fa70c558e544d590bdd9be747dc2de
Daniel Enriquez <daniel.enm17@gmail.com>
2022-02-03 14:14:23 -0600

    tests passed

--- 02-OOP/02-OO-Advanced/04-Instance-vs-Class/lib/restaurant.rb
+++ 02-OOP/02-OO-Advanced/04-Instance-vs-Class/lib/restaurant.rb
@@ -1,9 +1,22 @@
 class Restaurant
   # TODO: add relevant accessors if necessary
+  attr_reader :average_rating, :city
 
   def initialize(city, name)
     # TODO: implement constructor with relevant instance variables
+    @city = city
+    @name = name
+    @restaurant_rates = []
   end
 
   # TODO: implement .filter_by_city and #rate methods
+
+  def rate(new_rate)
+    @restaurant_rates << new_rate
+    @average_rating = @restaurant_rates.sum / @restaurant_rates.size
+  end
+
+  def self.filter_by_city(restaurants, city)
+    restaurants.select { |restaurant| restaurant.city == city }
+  end
 end
