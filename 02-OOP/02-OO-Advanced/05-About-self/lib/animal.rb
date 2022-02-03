5d726d0fa61d2638ea2cdb1d659b7a4a8e7482c6
Daniel Enriquez <daniel.enm17@gmail.com>
2022-02-03 16:40:58 -0600

    tests passed

--- 02-OOP/02-OO-Advanced/05-About-self/lib/animal.rb
+++ 02-OOP/02-OO-Advanced/05-About-self/lib/animal.rb
@@ -1,21 +1,25 @@
 class Animal
   def name(name)
     @name = name
+    self
     # TODO: what should this method return?
   end
 
   def species(species)
     @species = species
+    self
     # TODO: what should this method return?
   end
 
   def color(color)
     @color = color
+    self
     # TODO: what should this method return?
   end
 
   def natural_habitat(natural_habitat)
     @natural_habitat = natural_habitat
+    self
     # TODO: what should this method return?
   end
 
@@ -23,3 +27,7 @@ class Animal
     "Name: #{@name}, Species: #{@species}, Color: #{@color}, Natural Habitat: #{@natural_habitat}"
   end
 end
+
+cat = Animal.new
+cat.name("garfield").color("orange")
+p cat
