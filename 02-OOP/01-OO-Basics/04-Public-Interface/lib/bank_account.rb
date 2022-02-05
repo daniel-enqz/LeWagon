1b90fc4bb330221f67fc411615dbfc06a6d60d71
Daniel Enriquez <daniel.enm17@gmail.com>
2022-02-04 22:30:56 -0600

    test passed

--- 02-OOP/01-OO-Basics/04-Public-Interface/lib/bank_account.rb
+++ 02-OOP/01-OO-Basics/04-Public-Interface/lib/bank_account.rb
@@ -1,8 +1,13 @@
+require_relative 'transaction'
+require "pry-byebug"
+
 # This is how you define your own custom exception classes
 class DepositError < StandardError
 end
 
-class BankAccount
+class BankAccount < Transaction
+  attr_reader :name, :balance
+
   # Contract for the BankAccount class
   # - you can access full owner's name and balance, but partial IBAN
   # - you cannot access full IBAN
@@ -26,33 +31,39 @@ class BankAccount
   end
 
   def withdraw(amount)
-    # TODO: Call add_transaction with the right argument
-    # TODO: returns a string with a message
+    return "Yo’ve just withdrawn #{add_transaction(-amount)} euros"
   end
 
   def deposit(amount)
-    # TODO: Call add_transaction with the right argument
-    # TODO: returns a string with a message
+    return "Yo’ve just deposited #{add_transaction(amount)} euros"
   end
 
   def transactions_history(args = {})
-    # TODO: Check if there is a password and if so if it is correct
-    # TODO: return a string displaying the transactions, BUT NOT return the transaction array!
+    return "no password given" if args == {}
+    return @transactions.join if @password == args[:password]
+    return "wrong password" if @password != args[:password]
   end
 
   def iban
     # TODO: Hide the middle of the IBAN like FR14**************606 and return it
+    # regex = @iban.match(/(^\w{4})(.{26})(.{3})/)
+    @iban[4...-3] = "**************"
+    @iban
   end
 
   def to_s
     # Method used when printing account object as string (also used for string interpolation)
     # TODO: return a string with the account owner, the hidden iban and the balance of the account
+    return "#{@name}, #{iban}, #{@balance}"
   end
 
   private
 
   def add_transaction(amount)
     # TODO: add the amount in the transactions array
+    @transactions << amount
     # TODO: update the current balance (which represents the balance of the account)
+    @balance += amount
+    return amount
   end
 end
