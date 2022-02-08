61e5a35713060a9ae2733f207c0a0cc55d52c07a
Daniel Enriquez <daniel.enm17@gmail.com>
2022-02-08 14:37:43 -0600

    offenses corrected

--- 02-OOP/05-Food-Delivery-Day-One/01-Food-Delivery/test.rb
+++
@@ -1,7 +0,0 @@
-require_relative 'app/repositories/meal_repository'
-require_relative 'app/models/meal'
-
-csv_path = 'data/meals.csv'
-repo = MealRepository.new(csv_path)
-meal = Meal.new
-repo.create()
