6e098488d528f70229d2f15a814a5f820f43561c
Daniel Enriquez <daniel.enm17@gmail.com>
2022-02-11 09:46:25 -0600

    all passed

--- 03-AR-Database/02-SQL-CRUD/01-Query-the-DB/lib/query.rb
+++ 03-AR-Database/02-SQL-CRUD/01-Query-the-DB/lib/query.rb
@@ -3,4 +3,9 @@ require 'sqlite3'
 def all_artists(db)
   # TODO: use `db` to execute an SQL query against the database to
   # retrieve all columns from all records from the `artists` table.
+  rows = db.execute('SELECT * FROM artists')
+  return rows
 end
+
+# DB = SQLite3::Database.new("a_file.db")
+# rows = DB.execute('SELECT * FROM table_name')
