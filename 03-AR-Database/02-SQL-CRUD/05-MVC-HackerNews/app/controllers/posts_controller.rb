ec809aa5b04356f57338fda38214981662f48e9e
Daniel Enriquez <daniel.enm17@gmail.com>
2022-02-11 16:53:53 -0600

    all passed

--- 03-AR-Database/02-SQL-CRUD/05-MVC-HackerNews/app/controllers/posts_controller.rb
+++ 03-AR-Database/02-SQL-CRUD/05-MVC-HackerNews/app/controllers/posts_controller.rb
@@ -1,3 +1,7 @@
+require_relative "../models/post"
+require_relative "../views/posts_view"
+require 'pry-byebug'
+
 class PostsController
   def initialize
     @view = PostsView.new
@@ -9,21 +13,47 @@ class PostsController
 
   def index
     # TODO: implement listing all posts
+    all = Post.all
+    @view.display(all)
   end
 
   def create
     # TODO: implement creating a new post
+    title = @view.ask_for("title")
+    url = @view.ask_for("url")
+    votes = @view.ask_for("votes").to_i
+    post = Post.new(title: title, url: url, votes: votes)
+    post.save
   end
 
   def update
     # TODO: implement updating an existing post
+    index
+    all = Post.all
+    index_to_update = @view.ask_index
+    title = @view.ask_for("title")
+    url = @view.ask_for("url")
+    votes = all[index_to_update - 1].votes
+    post = Post.new(title: title, url: url, id: index_to_update, votes: votes)
+    post.save
   end
 
   def destroy
-    # TODO: implement destroying a post
+    index
+    index_to_update = @view.ask_index
+    post = Post.find(index_to_update)
+    post.destroy
   end
 
   def upvote
     # TODO: implement upvoting a post
+    index
+    all = Post.all
+    index_to_update = @view.ask_index
+    votes = all[index_to_update - 1].votes + 1
+    title = all[index_to_update - 1].title
+    url = all[index_to_update - 1].url
+    post = Post.new(title: title, url: url, id: index_to_update, votes: votes)
+    post.save
   end
 end
--- 03-AR-Database/02-SQL-CRUD/05-MVC-HackerNews/app/views/posts_view.rb
+++ 03-AR-Database/02-SQL-CRUD/05-MVC-HackerNews/app/views/posts_view.rb
@@ -1,3 +1,19 @@
+require 'pry-byebug'
 class PostsView
   # TODO: implement methods called by the PostsController
+  def ask_for(string)
+    puts "What's the #{string}"
+    print ">"
+    gets.chomp
+  end
+
+  def ask_index
+    puts "What's the index?"
+    print ">"
+    gets.chomp.to_i
+  end
+
+  def display(all)
+    all.each { |post| puts "-#{post.id}, TITLE:#{post.title}, URL:#{post.url}, VOTES:#{post.votes}" }
+  end
 end
