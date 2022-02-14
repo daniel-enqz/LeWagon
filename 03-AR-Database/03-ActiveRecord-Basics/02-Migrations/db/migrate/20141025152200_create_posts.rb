775be748a7beea011317b030650894d90de594a2
Daniel Enriquez <daniel.enm17@gmail.com>
2022-02-14 11:27:47 -0600

    tests passed

--- 03-AR-Database/03-ActiveRecord-Basics/02-Migrations/db/migrate/20141025152200_create_posts.rb
+++ 03-AR-Database/03-ActiveRecord-Basics/02-Migrations/db/migrate/20141025152200_create_posts.rb
@@ -1,5 +1,10 @@
 class CreatePosts < ActiveRecord::Migration[6.0]
+  # TODO: your code here to create the posts table
   def change
-    # TODO: your code here to create the posts table
+    create_table :posts do |p|
+      p.string :title
+      p.string :url
+      p.timestamps
+    end
   end
 end
