a05d1601c8fef2ae213821db3c186d5c06ba8d96
Daniel Enriquez <daniel.enm17@gmail.com>
2022-02-14 12:42:47 -0600

    tests passed

--- 03-AR-Database/03-ActiveRecord-Basics/05-Seed-API/db/seeds.rb
+++ 03-AR-Database/03-ActiveRecord-Basics/05-Seed-API/db/seeds.rb
@@ -1,4 +1,11 @@
 require "json"
 require "rest-client"
+require "pry-byebug"
 
-# TODO: Write a seed to insert 10 posts in the database fetched from the Hacker News API.
+10.times do |i|
+  response = RestClient.get "https://hacker-news.firebaseio.com/v0/item/#{i + 1}.json"
+  repos = JSON.parse(response)
+  # => repos is an `Array` of `Hashes`.
+  post = Post.create(title: repos["title"], url: repos["url"], votes: 2)
+  post.save!
+end
