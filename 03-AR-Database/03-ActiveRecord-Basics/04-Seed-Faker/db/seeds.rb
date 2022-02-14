c65a026d178aefef2cea1faa0c22a6475a87f659
Daniel Enriquez <daniel.enm17@gmail.com>
2022-02-14 12:15:15 -0600

    tests passed

--- 03-AR-Database/03-ActiveRecord-Basics/04-Seed-Faker/db/seeds.rb
+++ 03-AR-Database/03-ActiveRecord-Basics/04-Seed-Faker/db/seeds.rb
@@ -1,2 +1,6 @@
 require "faker"
 # TODO: Write a seed to insert 100 posts in the database
+100.times do
+  post = Post.create(title: Faker::Music.band, url: Faker::Sports::Football.player, votes: 2)
+  post.save!
+end
