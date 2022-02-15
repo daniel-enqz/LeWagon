3bbed6819e86f3ed1aef0e0453af339ff68d1116
Daniel Enriquez <daniel.enm17@gmail.com>
2022-02-14 20:16:44 -0600

    first tests passed

--- 03-AR-Database/03-ActiveRecord-Basics/05-Seed-API/db/seeds.rb
+++ 03-AR-Database/03-ActiveRecord-Basics/05-Seed-API/db/seeds.rb
@@ -1,11 +1,10 @@
 require "json"
 require "rest-client"
-require "pry-byebug"
 
 10.times do |i|
   response = RestClient.get "https://hacker-news.firebaseio.com/v0/item/#{i + 1}.json"
   repos = JSON.parse(response)
   # => repos is an `Array` of `Hashes`.
-  post = Post.create(title: repos["title"], url: repos["url"], votes: 2)
-  post.save!
+  post = Post.create(title: repos["title"], url: repos["url"], votes: repos["score"], id: repos["id"])
+  puts "[#{post.votes}] #{post.title} - #{post.url} - #{post.id}"
 end
