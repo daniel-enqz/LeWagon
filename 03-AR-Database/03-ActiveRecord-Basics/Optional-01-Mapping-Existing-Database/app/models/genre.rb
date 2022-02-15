0f3bbbeec54a309634d14de3a391c36b8e515ff8
Daniel Enriquez <daniel.enm17@gmail.com>
2022-02-14 21:02:52 -0600

    tests passed

--- 03-AR-Database/03-ActiveRecord-Basics/Optional-01-Mapping-Existing-Database/app/models/genre.rb
+++ 03-AR-Database/03-ActiveRecord-Basics/Optional-01-Mapping-Existing-Database/app/models/genre.rb
@@ -1,4 +1,8 @@
 class Genre < ActiveRecord::Base
   has_many :tracks
-  has_many :albums, through: :tracks
+  has_many :albums, -> { distinct }, through: :tracks
+  has_many :artists, -> { distinct }, through: :albums
 end
+
+# has_and_belongs_to_many :orders, join_table: 'books_orders'
+# has_many :authors, through: :books
--- 03-AR-Database/03-ActiveRecord-Basics/Optional-01-Mapping-Existing-Database/app/queries.rb
+++ 03-AR-Database/03-ActiveRecord-Basics/Optional-01-Mapping-Existing-Database/app/queries.rb
@@ -5,13 +5,20 @@ end
 
 def love_tracks
   # TODO: return an ActiveRecord::Relation (array) of Track which sings about Love
-  Track.where()
+  Track.where('name LIKE ?', '%love%')
 end
 
 def long_tracks(min_length)
   # TODO: return an ActiveRecord::Relation (array) of Track of at least `min_length` minutes
+  Track.where("milliseconds > #{min_length * 60_000}")
 end
 
 def top_five_artists(genre_name)
   # TODO: return the 5 artists with the most tracks of genre `genre_name`
+  Artist.select('artists.*, COUNT(tracks.name) AS track_count')
+        .joins(albums: { tracks: :genre })
+        .where(genres: { name: genre_name })
+        .group('artists.name')
+        .order('track_count DESC')
+        .limit(5)
 end
