10446bce9151e973a57e9bf81adcea47a7e0260f
Daniel Enriquez <daniel.enm17@gmail.com>
2022-02-15 11:22:06 -0600

    tests passed

--- 03-AR-Database/04-ActiveRecord-Advanced/01-Associations/db/seeds.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/01-Associations/db/seeds.rb
@@ -3,7 +3,7 @@ require "faker"
 require "pry-byebug"
 5.times do
   user = User.create(username: Faker::Games::Heroes.name, email: Faker::Internet.email)
-  7.times do
+  rand(5..10).times do
     post = Post.new(title: Faker::Quote.singular_siegler,
                     url: Faker::FunnyName.name,
                     votes: rand(0..1000))
