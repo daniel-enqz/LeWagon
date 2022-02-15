bc6d38821bbd10aa4fa8693863795cb173007447
Daniel Enriquez <daniel.enm17@gmail.com>
2022-02-15 12:26:12 -0600

    tests passed

--- 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/post.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/post.rb
@@ -1,5 +1,8 @@
 class Post < ActiveRecord::Base
   belongs_to :user
-
   # TODO: Add some validation
+  validates :user, :title, :url, presence: true
+  validates :url, format: { with: URI::DEFAULT_PARSER.make_regexp }
+  validates :title, length: { minimum: 5 }
+  validates :title, uniqueness: { case_sensitive: false }
 end
--- 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/user.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/user.rb
@@ -1,6 +1,20 @@
 class User < ActiveRecord::Base
   has_many :posts
-
   # TODO: Add some validation
+  validates :username, :email, presence: true
+  before_validation :strip_email
+  validates :email, format: { with: /\A.*@.*\.com\z/ }
+  validates :username, uniqueness: true
   # TODO: Add some callbacks
+  after_create :send_email
+
+  private
+
+  def strip_email
+    self.email = email.strip unless email.nil?
+  end
+
+  def send_email
+    FakeMailer.instance.mail("boris@lewagon.org", "Welcome to HN!")
+  end
 end
