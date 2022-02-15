5e2e48af48f17252f9da9d69f8590cafc9a11276
Daniel Enriquez <daniel.enm17@gmail.com>
2022-02-15 11:19:29 -0600

    tests passed

--- 03-AR-Database/04-ActiveRecord-Advanced/01-Associations/db/migrate/20141025152200_create_posts.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/01-Associations/db/migrate/20141025152200_create_posts.rb
@@ -1,9 +1,10 @@
 class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
-      t.string :title
-      t.string :url
-      t.integer :votes, default: 0
+      t.string      :title
+      t.string      :url
+      t.integer     :votes, default: 0
+      t.references  :user, foreign_key: true
       t.timestamps null: false
     end
   end
--- 03-AR-Database/04-ActiveRecord-Advanced/01-Associations/db/seeds.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/01-Associations/db/seeds.rb
@@ -1,1 +1,13 @@
 # TODO: Write a seed
+require "faker"
+require "pry-byebug"
+5.times do
+  user = User.create(username: Faker::Games::Heroes.name, email: Faker::Internet.email)
+  7.times do
+    post = Post.new(title: Faker::Quote.singular_siegler,
+                    url: Faker::FunnyName.name,
+                    votes: rand(0..1000))
+    post.user = user
+    post.save
+  end
+end
