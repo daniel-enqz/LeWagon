5f41d5c8ea87bb070ffb78ad680ae17f63a75a2c
Daniel Enriquez <daniel.enm17@gmail.com>
2022-02-21 16:08:12 -0600

    tests passed

--- 04-Front-End/04-JavaScript-basics/05-Lord-of-the-rings/lib/lord_of_the_rings.js
+++ 04-Front-End/04-JavaScript-basics/05-Lord-of-the-rings/lib/lord_of_the_rings.js
@@ -1,14 +1,36 @@
 const isGood = (soldierType) => {
-  // TODO: return `true` if the soldierType is Good.
-  // Hint: Hobbits, Elves, Dwarves and Eagles are Good
+  const good = ["Hobbits", "Elves", "Dwarves", "Eagles"];
+  return good.includes(soldierType);
 };
 
 const buildSoldierObject = (battlefield) => {
   // TODO: Given a battlefield (String), return an object of forces.
+  const RellenoDeJson = battlefield.replace(/([A-z-]+)/g, '"$1"');
+  const splitted = RellenoDeJson.split(",");
+  return JSON.parse(`{${RellenoDeJson}}`);
 };
 
 const whoWinsTheWar = (battlefield) => {
   // TODO: Based on the battlefield's description (it's a String), return "Good", "Evil" or "Tie".
+  let GoodSum = 0;
+  let EvilSum = 0;
+  const object = buildSoldierObject(battlefield);
+  const keys = Object.keys(object);
+  keys.forEach((key) => {
+    if (isGood(key)) {
+      GoodSum += object[key];
+    } else {
+      EvilSum += object[key];
+    }
+  });
+  let result;
+  if (GoodSum > EvilSum) {
+    result = "Good";
+  } else if (EvilSum > GoodSum) {
+    result = "Evil";
+  } else {
+    result = "Tie";
+  }
+  return result;
 };
-
 module.exports = { whoWinsTheWar, buildSoldierObject, isGood }; // Do not remove this line
