e6960ed5d63078cb7f1ea373b70d39325c88fa44
Daniel Enriquez <daniel.enm17@gmail.com>
2022-02-21 13:53:51 -0600

    tests passed

--- 04-Front-End/04-JavaScript-basics/04-Unordered-list/lib/list_generator.js
+++ 04-Front-End/04-JavaScript-basics/04-Unordered-list/lib/list_generator.js
@@ -1,9 +1,16 @@
 const listItem = (content) => {
   // TODO: return the proper <li> HTML tag with its content (as a string)
+  const templateFn = expression => `<li class="list-group-item">${content}</li>`;
+  return templateFn();
 };
 
 const unorderedList = (items) => {
   // TODO: return the proper <ul> markup (as a string)
+  const templateFn = expression => `<ul class="list-group">
+  ${items.map(item => `<li class="list-group-item">${item}</li>\n  `).join('')}</ul>`;
+  return templateFn();
 };
 
 module.exports = { listItem, unorderedList }; // Do not remove.
+
+console.log(unorderedList(['milk', 'butter', 'bread']));
