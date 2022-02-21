93d04dbd429a6097df6f258d8aba626bc87418e8
Daniel Enriquez <daniel.enm17@gmail.com>
2022-02-21 12:29:56 -0600

    tests passed

--- 04-Front-End/04-JavaScript-basics/03-Valid-email/lib/valid_email.js
+++ 04-Front-End/04-JavaScript-basics/03-Valid-email/lib/valid_email.js
@@ -1,5 +1,9 @@
 const valid = (email) => {
   // TODO: return true if the `email` string has the right pattern!
+  const validRegex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+  return validRegex.test(email);
 };
 
 module.exports = valid; // Do not remove.
+
+console.log(valid('boris.lewagon.org'));
