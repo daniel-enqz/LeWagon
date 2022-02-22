ab48ef959e77777f398672726e42623d1b0f7d60
Daniel Enriquez <daniel.enm17@gmail.com>
2022-02-22 09:04:44 -0600

    feture added

--- 04-Front-End/04-JavaScript-basics/06-Dataset/lib/dataset.js
+++ 04-Front-End/04-JavaScript-basics/06-Dataset/lib/dataset.js
@@ -1,5 +1,17 @@
 const dataset = (element) => {
   // TODO: return the element's data attributes in an object
+  const array = element.match(/data-\w+="\w+"/gm);
+  console.log(array);
+  return JSON.parse(`{${array}}`);
 };
 
 module.exports = dataset; // Do not remove.
+
+const burger = `<div class="card" data-id="42" data-price="15" data-category="popular">
+  <div class="card-category">Popular</div>
+  <div class="card-description">
+    <h2>The best burger in town (15€)</h2>
+  </div>
+</div>`;
+
+dataset(burger);
