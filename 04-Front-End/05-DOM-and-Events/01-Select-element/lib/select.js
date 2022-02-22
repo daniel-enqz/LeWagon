0fc7de1fd4d7cbb7d85b9901230b025acdbb3009
Daniel Enriquez <daniel.enm17@gmail.com>
2022-02-22 15:34:40 -0600

    passed

--- 04-Front-End/05-DOM-and-Events/01-Select-element/lib/select.js
+++ 04-Front-End/05-DOM-and-Events/01-Select-element/lib/select.js
@@ -1,11 +1,9 @@
 // TODO 1. Open the `index.html` file and set an id to the France <li>
 // (nothing to write in this file for this step)
 
-const franceElement = (element) => {
-  // TODO 2: select the France list item and return it!
-  console.log(element.innerText);
+const franceElement = () => {
+  const element = document.querySelector("#france");
+  return element;
 };
-const element = document.querySelector("#france");
-franceElement(element);
 
 module.exports = franceElement; // Do not remove.
