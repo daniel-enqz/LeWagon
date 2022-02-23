ec92636124449ab4b68c0db6b5d01e90c4b1141f
Daniel Enriquez <daniel.enm17@gmail.com>
2022-02-22 19:03:13 -0600

    test passed

--- 04-Front-End/05-DOM-and-Events/04-Wagon-Race/lib/wagon_race.js
+++ 04-Front-End/05-DOM-and-Events/04-Wagon-Race/lib/wagon_race.js
@@ -1,1 +1,21 @@
 // TODO: write your code here
+const moveForward = (player) => {
+  const wagon = document.querySelector(`#player${player}-race .active`);
+  if (wagon.nextElementSibling) {
+    wagon.nextElementSibling.classList.add('active');
+    wagon.classList.remove('active');
+  } else {
+    alert(`Player ${player} wins! Play again?`);
+    window.location.reload();
+  }
+};
+
+const moveWagons = (event) => {
+  if (event.key === "q") {
+    moveForward(1);
+  } else if (event.key === "p") {
+    moveForward(2);
+  }
+};
+
+document.addEventListener("keyup", moveWagons);
