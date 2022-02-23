bd0835690eaafb85677c67e53a82f2e2b894f3a3
Daniel Enriquez <daniel.enm17@gmail.com>
2022-02-23 14:57:06 -0600

    finished

--- 04-Front-End/06-HTTP-and-AJAX/03-AJAX-Autocomplete/lib/index.js
+++ 04-Front-End/06-HTTP-and-AJAX/03-AJAX-Autocomplete/lib/index.js
@@ -1,1 +1,19 @@
 // TODO: Autocomplete the input with AJAX calls.
+const search = document.querySelector("#search");
+const results = document.querySelector("#results");
+
+const updateSearch = (query) => {
+  fetch(`https://wagon-dictionary.herokuapp.com/autocomplete/${query}`)
+    .then(response => response.json())
+    .then((data) => {
+      data.words.forEach((element) => {
+        console.log(element);
+        results.insertAdjacentHTML("beforeend", `<li>${element}</li>`);
+      });
+    });
+};
+
+search.addEventListener("keyup", (event) => {
+  results.innerHTML = "";
+  updateSearch(search.value);
+});
