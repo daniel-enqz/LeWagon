3c2cee2faba34ee65fbce9e423a928925527a43e
Daniel Enriquez <daniel.enm17@gmail.com>
2022-02-22 18:03:18 -0600

    test passed

--- 04-Front-End/05-DOM-and-Events/03-My-First-Event-Listener/lib/listener.js
+++ 04-Front-End/05-DOM-and-Events/03-My-First-Event-Listener/lib/listener.js
@@ -1,1 +1,9 @@
 // TODO: React to a click on the button!
+document.querySelectorAll("button").forEach((button) => {
+  button.addEventListener("click", (event) => {
+    event.currentTarget.classList.toggle("disabled");
+    button.innerText = "Bingo!";
+    const audio = new Audio('audio_file.mp3');
+    audio.play();
+  });
+});
