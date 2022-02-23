7405ec180bb4937facb4606cefb6110a352eab42
Daniel Enriquez <daniel.enm17@gmail.com>
2022-02-23 10:57:46 -0600

    finished

--- 04-Front-End/06-HTTP-and-AJAX/01-Click-me/lib/index.js
+++ 04-Front-End/06-HTTP-and-AJAX/01-Click-me/lib/index.js
@@ -1,8 +1,10 @@
 const displayAlertOnButtonClick = () => {
   // TODO: Select the big green button
-
+  const button = document.querySelector(".btn");
   // TODO: Bind the `click` event to the button
-  // TODO: On click, display `Thank you!` in a JavaScript alert!
+  button.addEventListener("click", (event) => {
+    alert("Thank you!");
+  });
 };
 
 displayAlertOnButtonClick(); // Do not remove!
