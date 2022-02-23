08e716f65456fa9b13d5eb243586646dabcba84d
Daniel Enriquez <daniel.enm17@gmail.com>
2022-02-23 12:05:39 -0600

    finished

--- 04-Front-End/06-HTTP-and-AJAX/02-My-Favourite-Sports/lib/index.js
+++ 04-Front-End/06-HTTP-and-AJAX/02-My-Favourite-Sports/lib/index.js
@@ -1,1 +1,11 @@
-// TODO: write your code here!
+const buttons = document.querySelectorAll(".clickable");
+
+const toggleActiveClass = (event) => {
+  event.currentTarget.classList.toggle("active");
+};
+
+const bindSportToClick = (button) => {
+  button.addEventListener('click', toggleActiveClass);
+};
+
+buttons.forEach(bindSportToClick);
