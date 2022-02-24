f42d0fec5db7f01cd4661dd1e955630e974df074
Daniel Enriquez <daniel.enm17@gmail.com>
2022-02-24 16:23:02 -0600

    stimulus added

--- 04-Front-End/07-JavaScript-Plugins/05-Weather/lib/index.js
+++ 04-Front-End/07-JavaScript-Plugins/05-Weather/lib/index.js
@@ -1,1 +1,9 @@
-// TODO Set up Stimulus here
+import { Application } from "@hotwired/stimulus";
+import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers";
+
+window.Stimulus = Application.start();
+const context = require.context("./controllers", true, /\.js$/);
+// eslint-disable-next-line no-undef
+Stimulus.load(definitionsFromContext(context));
+
+https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b34baa16244d2b898ddd16abd48c7982
