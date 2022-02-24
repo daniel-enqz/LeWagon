7d1e376aff4385b7928c066b2d304c010d4c776b
Daniel Enriquez <daniel.enm17@gmail.com>
2022-02-24 15:48:42 -0600

    map added, all finished

--- 04-Front-End/07-JavaScript-Plugins/02-Geocoder/lib/controllers/display_coordinates_controller.js
+++ 04-Front-End/07-JavaScript-Plugins/02-Geocoder/lib/controllers/display_coordinates_controller.js
@@ -1,5 +1,5 @@
 import { Controller } from "@hotwired/stimulus";
-
+import mapboxgl from "mapbox-gl";
 export default class extends Controller {
   static targets = ["userInput", "coordinatesPlace"]
 
@@ -8,8 +8,20 @@ export default class extends Controller {
     fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${this.userInputTarget.value}.json?access_token=pk.eyJ1IjoiZGFuaWVsLWVucXoiLCJhIjoiY2t6c3Y4dXBsMDE0NTJ4bnh5eGV2MjI1YSJ9.xyKeXgAsjXLAXhwDsKw06w`)
       .then(response => response.json())
       .then((data) => {
-        const coordinatesTemplate = `Longitude: ${data.features[0].center[0]} Latitude: ${data.features[0].center[1]}`;
+        const longitude = data.features[0].center[0];
+        const latitude = data.features[0].center[1];
+        const coordinatesTemplate = `Longitude: ${longitude} Latitude: ${latitude}`;
         this.coordinatesPlaceTarget.innerText = coordinatesTemplate;
+        mapboxgl.accessToken = `pk.eyJ1IjoiZGFuaWVsLWVucXoiLCJhIjoiY2t6c3Y4dXBsMDE0NTJ4bnh5eGV2MjI1YSJ9.xyKeXgAsjXLAXhwDsKw06w`;
+        const map = new mapboxgl.Map({
+          container: "map",
+          style: "mapbox://styles/mapbox/streets-v9",
+          center: [longitude, latitude],
+          zoom: 12
+        });
+        new mapboxgl.Marker()
+          .setLngLat([longitude, latitude])
+          .addTo(map);
       });
   }
 }
