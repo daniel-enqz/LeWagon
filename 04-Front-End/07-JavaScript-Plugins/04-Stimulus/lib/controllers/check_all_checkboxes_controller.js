b675105646328d6936bf41b67f9d55a59483c5a2
Daniel Enriquez <daniel.enm17@gmail.com>
2022-02-24 12:48:16 -0600

    test passed

--- 04-Front-End/07-JavaScript-Plugins/04-Stimulus/lib/controllers/check_all_checkboxes_controller.js
+++ 04-Front-End/07-JavaScript-Plugins/04-Stimulus/lib/controllers/check_all_checkboxes_controller.js
@@ -4,6 +4,7 @@ export default class extends Controller {
   static targets = ["checkbox"]
 
   checkAll(event) {
+    console.log(event);
     if (event.target.checked === true) {
       this.checkboxTargets.forEach((element) => {
         element.checked = true;
