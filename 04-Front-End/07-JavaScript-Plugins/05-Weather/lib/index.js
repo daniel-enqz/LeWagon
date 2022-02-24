8d85179bb1c982ff727e1110889cd602a2871e8e
Daniel Enriquez <daniel.enm17@gmail.com>
2022-02-24 16:31:27 -0600

    stimulus added

--- 04-Front-End/07-JavaScript-Plugins/05-Weather/lib/index.js
+++ 04-Front-End/07-JavaScript-Plugins/05-Weather/lib/index.js
@@ -6,4 +6,4 @@ const context = require.context("./controllers", true, /\.js$/);
 // eslint-disable-next-line no-undef
 Stimulus.load(definitionsFromContext(context));
 
-https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b34baa16244d2b898ddd16abd48c7982
+// https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b34baa16244d2b898ddd16abd48c7982
