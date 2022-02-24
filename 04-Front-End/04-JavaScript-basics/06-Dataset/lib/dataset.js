a2c0ebaf5a64bbf25f85af3aa5a95381dce64e08
Daniel Enriquez <daniel.enm17@gmail.com>
2022-02-23 21:43:54 -0600

    finished

--- 04-Front-End/04-JavaScript-basics/06-Dataset/lib/dataset.js
+++ 04-Front-End/04-JavaScript-basics/06-Dataset/lib/dataset.js
@@ -1,17 +1,28 @@
+const cast = (data) => {
+  if (data === "true") {
+    return true;
+  }
+
+  if (data === "false") {
+    return false;
+  }
+
+  if ((data.match(/\d/g) || []).length === data.length) {
+    return Number.parseInt(data, 10);
+  }
+  return data;
+};
+
 const dataset = (element) => {
   // TODO: return the element's data attributes in an object
-  const array = element.match(/data-\w+="\w+"/gm);
-  console.log(array);
-  return JSON.parse(`{${array}}`);
+  const openingTag = element.split('>')[0];
+  const raw = openingTag.match(/data-\w+="\w+"/gm) || [];
+  const attributesObject = {};
+  raw.forEach((attribute) => {
+    const matchData = attribute.match(/data-(.+)="(.+)"/);
+    attributesObject[matchData[1]] = cast(matchData[2]);
+  });
+  return attributesObject;
 };
 
 module.exports = dataset; // Do not remove.
-
-const burger = `<div class="card" data-id="42" data-price="15" data-category="popular">
-  <div class="card-category">Popular</div>
-  <div class="card-description">
-    <h2>The best burger in town (15€)</h2>
-  </div>
-</div>`;
-
-dataset(burger);
