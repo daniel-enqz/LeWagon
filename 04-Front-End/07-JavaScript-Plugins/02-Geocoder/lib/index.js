2e1c125fb605d58bab279ef2c85c6d453f3d30a8
Daniel Enriquez <daniel.enm17@gmail.com>
2022-02-24 15:31:14 -0600

    working landl

--- 04-Front-End/07-JavaScript-Plugins/02-Geocoder/lib/index.js
+++ 04-Front-End/07-JavaScript-Plugins/02-Geocoder/lib/index.js
@@ -4,4 +4,5 @@ import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers";
 
 window.Stimulus = Application.start();
 const context = require.context("./controllers", true, /\.js$/);
+// eslint-disable-next-line no-undef
 Stimulus.load(definitionsFromContext(context));
