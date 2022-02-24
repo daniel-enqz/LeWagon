04e7cb5a050e63b93a38eb2d4c315bbdb40dd187
Daniel Enriquez <daniel.enm17@gmail.com>
2022-02-24 12:32:46 -0600

    test

---
+++ 04-Front-End/07-JavaScript-Plugins/04-Stimulus/lib/controllers/check_all_checkboxes_controller.js
@@ -0,0 +1,17 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  static targets = ["checkbox"]
+
+  checkAll(event) {
+    if (event.target.checked === true) {
+      this.checkboxTargets.forEach((element) => {
+        element.checked = true;
+      });
+    } else {
+      this.checkboxTargets.forEach((element) => {
+        element.checked = false;
+      });
+    }
+  }
+}
