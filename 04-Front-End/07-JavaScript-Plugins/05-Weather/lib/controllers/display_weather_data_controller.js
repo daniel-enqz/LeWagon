62db906c2a4e1e9aaff472aa8e5c7b71f5561cac
Daniel Enriquez <daniel.enm17@gmail.com>
2022-02-25 08:54:18 -0600

    finished

--- 04-Front-End/07-JavaScript-Plugins/05-Weather/lib/controllers/display_weather_data_controller.js
+++ 04-Front-End/07-JavaScript-Plugins/05-Weather/lib/controllers/display_weather_data_controller.js
@@ -3,22 +3,28 @@ import { Controller } from "@hotwired/stimulus";
 export default class extends Controller {
   static targets = ["name", "date", "temp", "description", "img", "userInput"]
 
+  initialize() {
+    this.apiKey = "7abddf8e22023d3c490ed409384cd672";
+  }
+
   connect() {
+    this.userInputTarget.value = "";
     navigator.geolocation.getCurrentPosition((info) => {
-      fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${info.latitude}&lon=${info.longitude}&appid=7abddf8e22023d3c490ed409384cd672`).then(response => response.json())
+      fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${info.coords.latitude}&lon=${info.coords.latitude}&units=metric&appid=${this.apiKey}`)
+        .then(response => response.json())
         .then((data) => { this.#fetchData(data); });
     });
   }
 
   display(event) {
     event.preventDefault();
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.userInputTarget}&units=metric&appid=7abddf8e22023d3c490ed409384cd672`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.userInputTarget.value}&units=metric&appid=${this.apiKey}`)
       .then(response => response.json())
       .then((data) => { this.#fetchData(data); });
   }
 
   #fetchData(data) {
-    this.nameTarget.innerText = data.name;
+    this.nameTarget.innerText = data.name || "Mexico";
     const dateTransformation = new Date(data.dt * 1000);
     this.dateTarget.innerText = dateTransformation.toString();
     this.descriptionTarget.innerText = data.weather[0].description;
