7593f9f243a163a7ae2caa99a653ac1340710cbe
Daniel Enriquez <daniel.enm17@gmail.com>
2022-02-25 01:05:49 -0600

    finished

--- 04-Front-End/07-JavaScript-Plugins/05-Weather/lib/controllers/display_weather_data_controller.js
+++ 04-Front-End/07-JavaScript-Plugins/05-Weather/lib/controllers/display_weather_data_controller.js
@@ -1,7 +1,28 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
+  static targets = ["name", "date", "temp", "description", "img", "userInput"]
+
   connect() {
-    console.log("connected");
+    navigator.geolocation.getCurrentPosition((info) => {
+      fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${info.latitude}&lon=${info.longitude}&appid=7abddf8e22023d3c490ed409384cd672`).then(response => response.json())
+        .then((data) => { this.#fetchData(data); });
+    });
+  }
+
+  display(event) {
+    event.preventDefault();
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.userInputTarget}&units=metric&appid=7abddf8e22023d3c490ed409384cd672`)
+      .then(response => response.json())
+      .then((data) => { this.#fetchData(data); });
+  }
+
+  #fetchData(data) {
+    this.nameTarget.innerText = data.name;
+    const dateTransformation = new Date(data.dt * 1000);
+    this.dateTarget.innerText = dateTransformation.toString();
+    this.descriptionTarget.innerText = data.weather[0].description;
+    this.tempTarget.innerText = data.main.temp.toString();
+    this.imgTarget.src = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
   }
 }
--- 04-Front-End/07-JavaScript-Plugins/05-Weather/lib/index.js
+++ 04-Front-End/07-JavaScript-Plugins/05-Weather/lib/index.js
@@ -5,5 +5,3 @@ window.Stimulus = Application.start();
 const context = require.context("./controllers", true, /\.js$/);
 // eslint-disable-next-line no-undef
 Stimulus.load(definitionsFromContext(context));
-
-// https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b34baa16244d2b898ddd16abd48c7982
