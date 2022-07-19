5271a81176219ed74dac7fa069453acd229ea011
Daniel Enriquez <daniel.enm17@gmail.com>
2022-07-19 16:46:00 -0500

    files added

--- 01-Ruby/03-Iterators-Blocks/Optional-01-Playing-with-strings/lib/palindrome.rb
+++ 01-Ruby/03-Iterators-Blocks/Optional-01-Playing-with-strings/lib/palindrome.rb
@@ -1,3 +1,5 @@
+require "pry-byebug"
 def palindrome?(a_string)
   # TODO: check if a_string is a palindrome
+  a_string.downcase.gsub(/[^a-z0-9]/, "") == a_string.downcase.gsub(/[^a-z0-9]/, "").reverse
 end
