b7637643e4895c731fbeee71448c84032578cf8b
Daniel Enriquez <daniel.enm17@gmail.com>
2022-07-19 15:56:55 -0500

    working

--- 01-Ruby/05-Regular-Expressions/03-Word-frequency/lib/word_frequency.rb
+++ 01-Ruby/05-Regular-Expressions/03-Word-frequency/lib/word_frequency.rb
@@ -1,17 +1,19 @@
-require "pry-byebug"
-
-def most_common_words(file_name, stop_words_file_name, number_of_word)
-  # TODO: return hash of occurences of number_of_word most frequent words
-  hash = {}
-  counter = 0
-  File.open(file_name, "r").each_line do |line|
-    line.split.each do |word|
-      return 0 if stop_words_file_name.include? word
-
-      hash[word] = 1
-    end
+def load_stop_words(stop_words_filename)
+  File.open(stop_words_filename, "r").map do |line|
+    line.chomp
   end
-  hash
 end
 
-p most_common_words("source-text.txt", "stop_words.txt", 3)
+def most_common_words(filename, stop_words_filename, number_of_word)
+  counter = Hash.new(0)
+
+  stop_words = load_stop_words(stop_words_filename)
+
+  File.open(filename, "r").each_line do |line|
+    line.chomp.downcase.split(/\W+/).each do |word|
+      counter[word] += 1 unless stop_words.include? word
+    end
+  end
+
+  counter.sort_by { |_, v| -v }[0..(number_of_word - 1)].to_h
+end
